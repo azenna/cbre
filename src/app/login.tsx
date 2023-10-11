1cc94e71997948a47c64e771ecd7d832c79e438f
Zenna Allwein <zrallwein@gmail.com>
2023-10-11 12:22:18 -0500

    alt

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <div className="flex flex-col w-fit items-center bg-slate-100 p-4 gap-2 shadow-md ">
       <h1 className="text-3xl">Welcome</h1>
-      <Image src="/login.png" width={300} height={400} />
+      <Image alt="corporate art" src="/login.png" width={300} height={400} />
       <form className="w-64">
         <input
           className="text-black p-1 border border-gray-500 focus:outline-none rounded-md"
